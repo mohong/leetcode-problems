426d4d8dc8a2c9ad0ba0bc50e9ae961e5dd8f53f
mohong <arthas_qin@qq.com>
2018-10-15 15:10:11 +0800

    新增"N叉树的前序遍历"题目

---
+++ 0589-N-ary Tree Preorder Traversal/src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        preorder(root, list);
+        return list;
+    }
+
+    private void preorder(Node node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        list.add(node.val);
+        for (Node child : node.children) {
+            preorder(child, list);
+        }
+    }
+}
